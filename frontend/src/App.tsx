3b87a376146fcdd9d47867dd5cee4ffb4894308a
fukata <tatsuya.fukata@gmail.com>
2019-09-12 20:06:51 +0700

    初回アクセス時にローディングページを表示し、ローカルストレージ上のデプロイ日時をチェックし古ければリロードするようにした。

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,35 +6,80 @@ import Archive from './Archive'
 import Sidebar from './Sidebar'
 import withProps from 'recompose/withProps'
 import ReactGA from 'react-ga'; 
+import ReactLoading from 'react-loading'
 import { db } from './firebase'
-import { firestore } from 'firebase'
 
-export default class extends React.Component<BrowserRouterProps> {
+interface State {
+  loaded: boolean 
+}
+export default class extends React.Component<BrowserRouterProps, State> {
+  constructor(props: BrowserRouterProps) {
+    super(props)
+    this.state = {
+      loaded: false
+    }
+  }
+
   componentDidMount() {
     const { pathname } = window.location;
     ReactGA.set({ page: pathname });
     ReactGA.pageview(pathname);
+
+    const key = 'settings.latestDeployedAt' 
+    const savedDeployedAt = localStorage.getItem(key)
+    if (savedDeployedAt === null) {
+      this.setState({
+        loaded: true
+      })
+    } else {
+      console.log(`savedDeployedAt=${savedDeployedAt}`)
+      db.collection('deploy_logs').doc('latest').get().then(doc => {
+        const data: any = doc.data()
+        const latestDeployedAt: firebase.firestore.Timestamp = data.deployedAt
+        const savedDeployedAtNumber = Number(savedDeployedAt)
+        if (latestDeployedAt.seconds > savedDeployedAtNumber) {
+          console.log(`Old app version so reload.`)
+          localStorage.setItem(key, latestDeployedAt.seconds.toString())
+          window.location.reload(true)
+        } else {
+          console.log(`Already latest app version.`)
+          this.setState({
+            loaded: true
+          })
+        }
+      })
+    }
   }
 
   render() {
-    const ArchiveWithProps = withProps(
-      (props: any) => ({
-        ...props,
-        tz: 'Asia/Tokyo'
-      }),
-    )(Archive)
-    return (
-      <div className="App container-fluid">
-        <div className="row">
-          <Sidebar />
-          <div className="main col-md-10">
-            <Switch>
-              <Route path="/" exact component={Chat} />
-              <Route path="/archives/:date" component={ArchiveWithProps} />
-            </Switch>
+    if (this.state.loaded) {
+      const ArchiveWithProps = withProps(
+        (props: any) => ({
+          ...props,
+          tz: 'Asia/Tokyo'
+        }),
+      )(Archive)
+      return (
+        <div className="App container-fluid">
+          <div className="row">
+            <Sidebar />
+            <div className="main col-md-10">
+              <Switch>
+                <Route path="/" exact component={Chat} />
+                <Route path="/archives/:date" component={ArchiveWithProps} />
+              </Switch>
+            </div>
           </div>
         </div>
-      </div>
-    )
+      )
+    } else {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <ReactLoading type={"bars"} color={"white"} />
+          </header>
+        </div>
+      )
+    }
   }
 }
