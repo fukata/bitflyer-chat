bfc06d5e5647f64a1d401b416a13ac17611becb8
fukata <tatsuya.fukata@gmail.com>
2019-09-19 15:58:34 +0700

    アドセンスにNo Contentと言われたので排除した。

--- frontend/src/Sidebar.tsx
+++ frontend/src/Sidebar.tsx
@@ -2,14 +2,12 @@ import React from "react"
 import './Sidebar.css'
 import Calendar from './Calendar'
 import Settings from './Settings'
-import Adsense from './Adsense'
 
 export default class extends React.Component {
   render() {
     return (
       <div className="sidebar col-lg-2 d-none d-lg-block">
         <div className="sidebar-inner">
-          <Adsense />
           <Calendar />
           <Settings />
         </div>
